421be0ae0decf0e8e3aac6a1a457dfe2e4edb95c
reubenmiller <reuben.d.miller@gmail.com>
2025-02-23 22:50:32 +0100

    build for other targets

--- test/cross-compile.sh
+++ test/cross-compile.sh
@@ -5,6 +5,12 @@
 
 set -xeuf -o pipefail
 
+# x86_64 (host)
+cargo build --features "pkcs11-provider, mbed-crypto-provider, tpm-provider, unix-peer-credentials-authenticator, direct-authenticator" \
+	--release \
+	--target x86_64-unknown-linux-gnu \
+	--config 'target.x86_64-unknown-linux-gnu.linker="x86_64-linux-gnu-gcc"'
+
 # The "jwt-svid-authenticator" feature is not included yet because of a cross compilation
 # problem of BoringSSL. See https://github.com/tikv/grpc-rs/issues/536. Once resolved,
 # "all-authenticators" will be used again.
@@ -19,14 +25,30 @@ export SYSROOT=/tmp/arm-linux-gnueabihf
 # Add the correct libcrypto to the linking process
 export RUSTFLAGS="-lcrypto -L/tmp/arm-linux-gnueabihf/lib"
 cargo build --features "pkcs11-provider, mbed-crypto-provider, tpm-provider, unix-peer-credentials-authenticator, direct-authenticator" \
+	--release \
 	--target armv7-unknown-linux-gnueabihf \
 	--config 'target.armv7-unknown-linux-gnueabihf.linker="arm-linux-gnueabihf-gcc"'
 
+# ---------------------------------
+# target: arm-unknown-linux-gnueabi
+# ---------------------------------
+#
+# Set the SYSROOT used by pkg-config
+export SYSROOT=/tmp/arm-linux-gnueabi
+# Add the correct libcrypto to the linking process
+export RUSTFLAGS="-lcrypto -L/tmp/arm-linux-gnueabi/lib"
+cargo build --features "pkcs11-provider, mbed-crypto-provider, tpm-provider, unix-peer-credentials-authenticator, direct-authenticator" \
+	--release \
+	--target arm-unknown-linux-gnueabi \
+	--config 'target.arm-unknown-linux-gnueabi.linker="arm-linux-gnueabi-gcc"'
+
+
 export SYSROOT=/tmp/aarch64-linux-gnu
 export RUSTFLAGS="-lcrypto -L/tmp/aarch64-linux-gnu/lib"
-# Pull in the TS code
-git submodule update --init
+# Pull in the TS code (but don't fail if it does not work)
+git submodule update --init || echo "Warning: Failed to update submodule"
 cargo build --features "pkcs11-provider, mbed-crypto-provider, tpm-provider, trusted-service-provider, unix-peer-credentials-authenticator, direct-authenticator" \
+	--release \
 	--target aarch64-unknown-linux-gnu \
 	--config 'target.aarch64-unknown-linux-gnu.linker="aarch64-linux-gnu-gcc"'
 
@@ -34,4 +56,17 @@ cargo build --features "pkcs11-provider, mbed-crypto-provider, tpm-provider, tru
 apt install -y libc6-dev-i386-amd64-cross
 export SYSROOT=/tmp/i686-linux-gnu
 export RUSTFLAGS="-lcrypto -L/tmp/i686-linux-gnu/lib"
-cargo build --features "pkcs11-provider, mbed-crypto-provider, tpm-provider, unix-peer-credentials-authenticator, direct-authenticator, tss-esapi/generate-bindings" --target i686-unknown-linux-gnu
+cargo build --release --features "pkcs11-provider, mbed-crypto-provider, tpm-provider, unix-peer-credentials-authenticator, direct-authenticator, tss-esapi/generate-bindings" --target i686-unknown-linux-gnu
+
+# -----------------------------------
+# target: riscv64gc-unknown-linux-gnu
+# -----------------------------------
+#
+# Set the SYSROOT used by pkg-config
+export SYSROOT=/tmp/arm-linux-gnueabi
+# Add the correct libcrypto to the linking process
+export RUSTFLAGS="-lcrypto -L/tmp/arm-linux-gnueabi/lib"
+cargo build --features "pkcs11-provider, mbed-crypto-provider, tpm-provider, unix-peer-credentials-authenticator, direct-authenticator" \
+	--release \
+	--target riscv64gc-unknown-linux-gnu \
+	--config 'target.riscv64gc-unknown-linux-gnu.linker="riscv64-linux-gnu-gcc"'
